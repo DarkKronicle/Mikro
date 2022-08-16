e35351ae81a37593fcd61016ee8616016ce1fc29
DarkKronicle <darkkronicle@gmail.com>
2022-08-16 11:28:05 -0600

    HP system

---
+++ bot/cogs/chat.py
@@ -0,0 +1,51 @@
+import json
+from datetime import datetime
+
+from discord.ext import commands
+import discord
+from bot.mikro import Mikro
+import traceback
+
+
+class Chat(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot: Mikro = bot
+        self.bot.add_loop('status', self.update_status)
+        self.score = 0
+
+    async def cog_load(self) -> None:
+        try:
+            with open('storage.json', 'r') as f:
+                self.score = json.load(f)['chat_score']
+        except:
+            pass
+
+    async def cog_unload(self) -> None:
+        try:
+            with open('storage.json', 'w') as f:
+                json.dump({'chat_score': self.score}, f, indent=4)
+        except Exception as e:
+            traceback.print_exc()
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        self.score += 1
+
+    async def update_status(self, time: datetime):
+        if time.minute % 5 == 0:
+            self.score -= 1
+            await self.set_status()
+
+    async def set_status(self):
+        activity = discord.Activity(
+            type=discord.ActivityType.playing,
+            name='with {0} chat HP'.format(self.score),
+        )
+        await self.bot.change_presence(status=discord.Status.online, activity=activity)
+
+
+async def setup(bot):
+    await bot.add_cog(Chat(bot))
--- bot/mikro.py
+++ bot/mikro.py
@@ -1,3 +1,4 @@
+import math
 import typing
 
 import bot as bot_global
@@ -5,8 +6,9 @@ import logging
 import traceback
 
 import discord
-from discord.ext import commands
-from datetime import datetime
+from discord.ext import commands, tasks
+from datetime import datetime, timedelta
+from pytz import timezone
 
 from bot import response
 from bot.core.context import Context
@@ -20,8 +22,32 @@ startup_extensions = (
     'bot.cogs.voice',
     'bot.cogs.conversation',
     'bot.cogs.minecraft',
+    'bot.cogs.chat',
     'bot.response.response_cog',
 )
+
+
+def get_time_until_minute():
+    return 60 - datetime.now().second
+
+
+def round_time(time_object=None, round_to=30 * 60):
+    """
+    Round a datetime object to any time lapse in seconds
+    dt : datetime.datetime object, default now.
+    roundTo : Closest number of seconds to round to, default 1 minute.
+    Author: Thierry Husson 2012 - Use it as you want but don't blame me.
+    """
+    if time_object is None:
+        zone = timezone('UTC')
+        utc = timezone('UTC')
+        time_object = utc.localize(datetime.now())
+        time_object = time_object.astimezone(zone)
+
+    stripped_dt = time_object.replace(tzinfo=None, hour=0, minute=0, second=0)
+    seconds = (time_object.replace(tzinfo=None) - stripped_dt).seconds
+    rounding = (seconds + round_to / 2) // round_to * round_to
+    return time_object + timedelta(0, rounding - seconds, -time_object.microsecond)
 
 
 class Mikro(commands.Bot):
@@ -47,6 +73,7 @@ class Mikro(commands.Bot):
             tags=False,
         )
         self.boot = datetime.now()
+        self.loops = {}
 
     def get_main_guild(self):
         return self.get_guild(753693459369427044)
@@ -64,8 +91,45 @@ class Mikro(commands.Bot):
     def run(self):
         super().run(bot_global.config['bot_token'], reconnect=True)
 
+    def add_loop(self, name, function):
+        """
+        Adds a loop to the thirty minute loop. Needs to take in a function with a parameter time with async.
+        """
+        self.loops[name] = function
+
+    def remove_loop(self, name):
+        """
+        Removes a loop based off of a time.
+        """
+        if name in self.loops:
+            self.loops.pop(name)
+
+    @tasks.loop(minutes=1)
+    async def time_loop(self):
+        time = round_time(round_to=60)
+        for _, function in self.loops.items():
+            try:
+                await function(time)
+            except Exception as error:
+                if isinstance(error, (discord.Forbidden, discord.errors.Forbidden)):
+                    return
+                traceback.print_exc()
+
+    first_loop = True
+
+    @tasks.loop(seconds=get_time_until_minute())
+    async def setup_loop(self):
+        # Probably one of the most hacky ways to get a loop to run every thirty minutes based
+        # off of starting on one of them.
+        if Mikro.first_loop:
+            Mikro.first_loop = False
+            return
+        self.time_loop.start()
+        self.setup_loop.stop()
+
     async def run_once_when_ready(self):
         await self.wait_until_ready()
+        self.setup_loop.start()
         print('Ready!')
 
     async def on_command_error(self, ctx, error, *, raise_err=True):  # noqa: WPS217
